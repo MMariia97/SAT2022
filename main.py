ec255d2cccb98b0705fade5d706ff837a83e0ad2
MMariia97 <109080006+MMariia97@users.noreply.github.com>
2022-10-04 20:15:54 +0300

    Add simulated and quantum annealing

--- main.py
+++ main.py
@@ -7,49 +7,53 @@ import sympy as sym
 import math
 import multiprocessing
 from pulp import *
+import neal
+import dimod
+import json
+from dwave.system import DWaveSampler, EmbeddingComposite
 
 def sat1(x):
-    return not x[0] and not x[1] and not x[2] and not x[3] and not x[4] and x[5] and not x[6] and x[7] and x[8] and not x[9] and x[10] and x[11] and x[12] and x[13] and x[14] and not x[15]
+  return not x[0] and not x[1] and not x[2] and not x[3] and not x[4] and x[5] and not x[6] and x[7] and x[8] and not x[9] and x[10] and x[11] and x[12] and x[13] and x[14] and not x[15]
 
 def convert_to_minus(x):
-    # 0 -> 1, 1 -> -1
-    return (1 - 2*numpy.multiply(x, 1))
+  # 0 -> 1, 1 -> -1
+  return (1 - 2*numpy.multiply(x, 1))
 
 N=16
-J = numpy.array(sym.Matrix(N, N, lambda i,j: sym.Symbol('J_%d,%d' % (i, j))).tolist())
 
 def getEquations(*x):
-  a = numpy.zeros(N*(N+1)+1)
+  a = numpy.zeros(N*(N+1)+2)
   for i in range(N):
     a[i] = convert_to_minus(x[i]) # find coefficients for h_i
-  for row in range(len(J)):
-    for col in range(len(J[row])):
-      if (row < col):
-        a[col + row*N + N] = convert_to_minus(x[row])*convert_to_minus(x[col]) # find coefficients for J_ij, i<j
-      else:
-        a[col + row*N + N] = 0
-  sat = sat1(x)
-  if (sat == True):
-    a[N*(N+1)] = 0
-    return a
-  else:
-    a[N*(N+1)] = -1 # find coefficients for g
+    for row in range(N):
+      for col in range(N):
+        if (row < col):
+          a[col + row*N + N] = convert_to_minus(x[row])*convert_to_minus(x[col]) # find coefficients for J_ij, i<j
+        else:
+          a[col + row*N + N] = 0
+    sat = sat1(x)
+    if (sat == True):
+      a[N*(N+1)] = 0
+    else:
+      a[N*(N+1)] = -1 # find coefficients for g
+    a[N*(N+1)+1] = -1 # coefficient for k
     return a
 
 def main():
   num_cores = multiprocessing.cpu_count()
   pool = multiprocessing.Pool(num_cores)
-  res = pool.starmap_async(getEquations, list(itertools.product([False, True], repeat=16)), chunksize=10)
+  res = pool.starmap_async(getEquations, list(itertools.product([False, True], repeat=N)), chunksize=10)
   pool.close()
   pool.join()
   eq_arr = res.get()
 
-  prob = LpProblem("Hamiltonian_coeff", LpMinimize)
+  prob = LpProblem("Hamiltonian_coeff", LpMaximize)
   h = LpVariable.dicts("h", range(0, N), lowBound=-2, upBound=2)
   JJ = LpVariable.dicts("JJ", range(N, N**2+N), lowBound=-1, upBound=1)
   g = LpVariable.dicts("g", range(N+N**2, N+N**2+1), lowBound=0)
-  prob += (-1)*g[N+N**2]
-  total_dict = {**h, **JJ, **g} # variables
+  k = LpVariable.dicts("k", range(N+N**2+1, N+N**2+2))
+  prob += g[N+N**2]
+  total_dict = {**h, **JJ, **g, **k} # variables
   var_list = list(total_dict.values())
   print("var_list", var_list)
   prod = numpy.matmul(eq_arr,var_list)
@@ -64,18 +68,44 @@ def main():
       prob += LpConstraint(prod[i],rhs=0,sense = 0)
   status = prob.solve()
 
+  J_dict = {}
+  h_dict = {}
   print("status\n") 
   print(LpStatus[status])
-  print("\nh\n")
+  print("\nh_dict\n")
   for i in (range(len(h))):
-    print(h[i].varValue)
-  print("\nJJ\n")
+    if (h[i].varValue != None):
+      h_dict[i] = h[i].varValue
+    else:
+      h_dict[i] = 0
+  print(h_dict)
+  print("\nJ_dict\n")
   for i in range(N):
     for j in range(N):
-      print(JJ[i*N+j+N].varValue)
-    print("\n")
+      if (i<j):
+        J_dict[(i, j)] = JJ[i*N+j+N].varValue
+  print(J_dict)
   print("\ng\n")
   print(g[N+N**2].varValue)
+  print("\nk\n")
+  print(k[N+N**2+1].varValue)
+
+  sampler = neal.SimulatedAnnealingSampler()
+
+  sampleset = sampler.sample_ising(h_dict, J_dict, num_reads=100)
+  print("sampleset\n", sampleset)
+
+  print("\nto qubo")
+  print(dimod.ising_to_qubo(h_dict, J_dict))
+
+  ### DWaveSolver
+ # sampler = EmbeddingComposite(DWaveSampler())
+
+ # sampleset = sampler.sample_ising(h_dict, J_dict, num_reads=100)
+ # print("sampleset\n", sampleset)
+
+ # print("\nto qubo")
+ # print(dimod.ising_to_qubo(h_dict, J_dict))
 
   return 0
 
